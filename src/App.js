25fb681221ce66b1ab1fa0a23d95d0a84a1aabf7
unknown <kelvinbusiness10@gmail.com>
2022-11-11 11:00:15 +0700

    [ADD] styling foundation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
 import logo from './logo.svg';
-import './App.css';
+import './Login.css';
+import icon from './asset_icon.png';
+import logonarindo from './narindo.png';
+import { Carousel} from 'react-responsive-carousel'
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <div className='white-container'>
+        <div className='left-container'>
+          <div className='carousel-div'>
+          <Carousel className='left-container' showThumbs={false} autoPlay infiniteLoop
+          showStatus={false} emulateTouch showArrows={false}>
+            <div>
+              <img style={{marginBottom: '40px'}} src={icon} width={'50%'} height={'50%'}></img>
+            </div>
+            <div>
+              <img src={icon} width={'50%'} height={'50%'}></img>
+            </div>
+
+          </Carousel>
+          </div>
+        </div>
+        <div className='right-container'>
+          <img src={logonarindo} width={'40%'} height={'12%'}></img>
+          <h1>Welcome to Namaste!</h1>
+          <p id='login-txt'>Login to your account</p>
+          <p id='email'>Email</p>
+          <input className='input'></input>
+          <button className='login-btn'><span>Login</span></button>
+        </div>
+      </div>
     </div>
   );
 }
